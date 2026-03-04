fbcc4b8127396b856ef2480250730f670037cb58
Charles P. Mineh <charlespmineh10@gmail.com>
2026-03-04 15:51:45 +0000

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'mineh-quiz-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
